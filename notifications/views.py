1e6175815035e05d0b1ae38ad4692d31a29f8e77
dinobox <dinobox.create@gmail.com>
2022-07-11 14:32:54 +0900

    ADD: 알림 정보 관련
    고객 알림
    딜러 알림
    구현

--- notifications/views.py
+++ notifications/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+import json, jwt
+from json.decoder import JSONDecodeError
 
-# Create your views here.
+from django.db.models import Sum
+from django.http  import JsonResponse
+from django.db import transaction
+from django.views import View
+
+from kolon_wecode.settings  import SECRET_KEY, ALGORITHM
+from cores.utils import login_decorator, admin_login_decorator
+from cars.models import Car, InsuranceHistory, TransactionHistory
+from testcar.models import TestCar
+from estimates.models import Estimate, EstimateCarImage
+from notifications.models import SalesProcess, QuoteNotification, UserNotification
+
+class QuoteNotificationView(View):
+    # admin 알람 기능 / 딜러 지정 되지 않은 내역만 필터링
+    @admin_login_decorator
+    def get(self, request):
+        notifications = QuoteNotification.objects.filter(branch_id=request.dealer.branch_id, dealer_assign = False)
+        
+        results = [{
+            'branch'       : notification.branch.name,
+            'car_number'   : notification.sales_process.estimate.car.car_number,
+            'dealer_assign': notification.dealer_assign,
+        }for notification in notifications]
+        
+        
+        return JsonResponse({'results': results}, status=200)
+
+class UserNotificationView(View):
+    # 고객 알람 기능 
+    @login_decorator
+    def get(self, request):
+        car        = request.car
+        notifications = car.estimate_set.all()[0].salesprocess_set.all()[0].usernotification_set.all()
+        
+        results = [{
+            'notification_id': notification.id,
+            'car_number'     : notification.sales_process.estimate.car.car_number,
+            'car_number'     : notification.sales_process.estimate.car.car_number,
+            'content'        : notification.content,
+            'read'           : notification.read,
+            'create_at'      : notification.created_at,
+        }for notification in notifications]
+        
+        return JsonResponse({'results': results}, status=200)
+    
+    @login_decorator
+    def patch(self, request):
+        car           = request.car
+        notifications = car.estimate_set.all()[0].salesprocess_set.all()[0].usernotification_set.all()
+        
+        results = [{
+            'car_number': notification.sales_process.estimate.car.car_number,
+            'content'   : notification.content,
+            'create_at' : notification.created_at,
+        }for notification in notifications]
+        
+        return JsonResponse({'results': results}, status=200)
+
