d4e642ec6954eb2d4b7ebdb3a97cf3532dfd157f
dinobox <dinobox.create@gmail.com>
2022-07-12 21:51:34 +0900

    FIX: 수정중

--- kolon_wecode/settings.py
+++ kolon_wecode/settings.py
@@ -1,10 +1,10 @@
 import os
-import pymysql
+# import pymysql
 import environ
 
 from pathlib import Path
 
-pymysql.install_as_MySQLdb()
+# pymysql.install_as_MySQLdb()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
